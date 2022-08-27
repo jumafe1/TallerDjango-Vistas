ed03e73eeebbf824f08641cff031ca26e8077b13
Julián <jm.ferreira@uniandes.edu.co>
2022-08-26 23:05:00 -0500

    database

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "TALLER3",
+        "USER": "jumafe",
+        "PASSWORD": "Juli2010",
         "HOST": "localhost",
         "PORT": "",
     }
